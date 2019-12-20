6dd00d9227072b44bea5b69741807833fa609910
kian behzad <kian.behzad@gmail.com>
2019-12-20 12:47:12 +0330

    [feature] add plot_data request

--- server/myserver/plotter/urls.py
+++ server/myserver/plotter/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_data/', views.data),
+    path('get_data/', views.get_data),
+    path('plot_data/', views.plot_data),
     path('', views.plotter),
 ]
--- server/myserver/plotter/views.py
+++ server/myserver/plotter/views.py
@@ -5,6 +5,9 @@ from .models import Experiment, Properties
 import json
 from .proto import messages_motor_pb2
 import socket
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import os
 # Create your views here.
 
 
@@ -41,7 +44,7 @@ def plotter(request):
     return HttpResponse("speed {}, position {}".format(speed, position))
 
 
-def data(request):
+def get_data(request):
     pk = request.GET.get('pk')
     pk = -1 if pk == None else int(pk)
     if pk == -1:
@@ -61,3 +64,40 @@ def data(request):
 
     return HttpResponse(json.dumps(data_dict), content_type="application/json")
 
+def plot_data(request):
+    pk = request.GET.get('pk')
+    pk = -1 if pk == None else int(pk)
+    if pk == -1:
+        return HttpResponse("NO PK")
+
+    the_exp = None
+    for exp in Experiment.objects.all():
+        if exp.pk == pk:
+            the_exp = exp
+    if the_exp is None:
+        return HttpResponse("INVALID PK")
+
+    data_dict = {"speeds": [], 'positions': []}
+    for prop in the_exp.all_properties.all():
+        data_dict["speeds"].append(prop.speed)
+        data_dict["positions"].append(prop.position)
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    # time = [i for i in range(len(data_dict["speeds"]))]
+    data_size = len(data_dict["speeds"])
+    ax.plot(data_dict["speeds"], 'r', label="speed")
+    ax.plot(data_dict["positions"], 'b', label="position")
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(data_size/500))
+    # ax.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+    # ax.yaxis.set_major_locator(ticker.MultipleLocator(data_size/100))
+    # ax.yaxis.set_minor_locator(ticker.MultipleLocator(5))
+    plt.title('Plot')
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow = True, ncol = 2)
+    ax.grid()
+    # plt.show()
+    filename = the_exp.__str__() + '.svg'
+    plt.savefig(filename)
+    svg_data = open(the_exp.__str__() + '.svg')
+    os.remove(filename)
+    return HttpResponse(svg_data, content_type="image/svg+xml")
